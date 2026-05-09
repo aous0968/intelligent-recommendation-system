1cd35d53cec9e52d2b062caa10c1e6a4c7de235a
aous0968 <aous0968@github.com>
2026-05-09 20:29:07 +0300

    fixing templates

--- smart_recommendation/settings.py
+++ smart_recommendation/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'smart_recommendation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates'],
+        'DIRS': ['/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
